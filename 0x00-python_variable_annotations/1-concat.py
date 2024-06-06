1262c1e831c27bcd33f20c1b229df06296c21b9a
root <abdezaad>
2024-06-06 06:33:41 -0700

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,7 @@
 and a string str2 as arguments and returns a concatenated string
 """
 
+
 def concat(str1: str, str2: str) -> str:
-    """Return the concatenated string of two strings"""
+    """Concatenate two strings"""
     return str1 + str2
